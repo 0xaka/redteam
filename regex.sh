00dbdca21e4bfeeae2c34b17521d81f11de782c9
0xaka <34306902+0xaka@users.noreply.github.com>
2018-05-16 17:19:50 +0200

    Update regex.sh

--- regex.sh
+++ regex.sh
@@ -1,1 +1,3 @@
 grep -P '^[^/]*/[^/]*$' # only one / by line
+sort daily-dn-union.txt | uniq -u > daily-dn.txt # sort and deduplicate file
+awk 'BEGIN { FS = "," } ; { print $3 }' < $f | sed s/\"//g >> /cygdrive/f/domain-names/daily-dn5.txt # read csv & remove quotes 
